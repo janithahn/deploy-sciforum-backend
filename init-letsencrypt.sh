7ee7e120c0dc7baf0747bd6fc706d053e0c7a042
Janitha Nawarathna <64242868+s16417@users.noreply.github.com>
2021-01-27 08:32:19 +0530

    emsil address for init-letsencrypt.sh file

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(api.sciforum.tech www.api.sciforum.tech)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="admin@sciforum.tech" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
